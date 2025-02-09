fe5de33192a33fb25d71af19edf0cc093814365b
Klimondra <klimondra@seznam.cz>
2025-02-09 20:48:28 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Klimondra web
+Web se právě připravuje
+
+### Po dobu přípravy webu stále funguje web [ZDE](https://klimondra.github.io/)
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+          <h2>Hello World</h2>
+          <h5>Zde bude vznikat nový web....</h5>
+        </div>
+    );
+};
+
+export default App;

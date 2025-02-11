defd0aab336ef2ec45b954009a83761f101da220
Klimondra <klimondra@seznam.cz>
2025-02-11 20:56:06 +0100

    Added responsive navbar
    Took 1 hour 4 minutes

--- src/components/global/Navbar.js
+++ src/components/global/Navbar.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {NavLink} from "react-router-dom";
 import "./Navbar.css"
 import {Link} from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Navbar = () => {
+    const [openMenu, setOpenMenu] = useState(false)
+
     return (
         <nav>
-            <Link to="/" id="backToMain">Klimondra</Link>
-            <div>
+            <div id="logoAndIco">
+                <Link to="/" id="backToMain">Klimondra</Link>
+                <GiHamburgerMenu className="menuBtn" onClick={() => {setOpenMenu(!openMenu)}}/>
+            </div>
+            <div className={openMenu ? "menu open" : "menu close"}>
                 <NavLink to="/" className="navLink">O mně</NavLink>
                 <NavLink to="/projekty" className="navLink">Projekty</NavLink>
                 <NavLink to="/nikam" className="navLink">Kontakt</NavLink>

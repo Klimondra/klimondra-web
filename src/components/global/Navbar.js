2c5fa0f0632e3c3f1d289455c2a1513aba0f51ef
Klimondra <klimondra@seznam.cz>
2025-02-11 13:26:02 +0100

    Added Nav Bar
    Took 40 minutes

--- src/components/global/Navbar.js
+++ src/components/global/Navbar.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import {NavLink} from "react-router-dom";
+import "./Navbar.css"
 
 const Navbar = () => {
     return (
         <nav>
-
+            <h2>Klimondra</h2>
+            <div>
+                <NavLink to="/" className="navLink">O mně</NavLink>
+                <NavLink to="/projekty" className="navLink">Projekty</NavLink>
+                <NavLink to="/nikam" className="navLink">Kontakt</NavLink>
+            </div>
         </nav>
     );
 };

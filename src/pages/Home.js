63b04ef088264ba166bc60c66c35be72669d1ce8
Klimondra <klimondra@seznam.cz>
2025-02-11 17:04:42 +0100

    Added typing effect to hero section
    Took 13 minutes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import "./Home.css";
 import AnimatedDotBg from "../components/homePage/animatedDotBg";
+import { TypeAnimation } from 'react-type-animation';
 
 const Home = () => {
-
 
     return (
         <>
@@ -12,7 +12,11 @@ const Home = () => {
                 <div className="heroContent">
                     <h3>Ahoj, já jsem</h3>
                     <h1>Klimondra</h1>
-                    <p>Dělám weby a software...</p>
+                    <p><TypeAnimation sequence={[
+                        "Dělám weby", 3000,
+                        "Dělám software", 3000,
+                        "Dělám grafiku", 750,
+                        "Dělám grafiku (občas)", 2250 ]} wrapper="span" speed={32}  repeat={Infinity}/></p>
                     <div className="heroCtaBtns">
                         <a href="#">Zjistit více</a>
                         <a href="#">Kontakt</a>

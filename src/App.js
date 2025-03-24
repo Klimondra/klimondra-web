bba84e7e55685abfd9840b8aad95c8afdd557152
Klimondra <klimondra@seznam.cz>
2025-03-24 20:33:14 +0100

    Added single project page and descriptions

--- src/App.js
+++ src/App.js
@@ -7,18 +7,33 @@ import Navbar from "./components/global/Navbar";
 import Footer from "./components/global/footer";
 import Kontakt from "./pages/Kontakt";
 import Projekty from "./pages/Projekty";
+import ProjectPage from "./pages/ProjectPage";
 
 const App = () => {
     return (
         <BrowserRouter>
             <Navbar/>
             <Routes>
+                { /*** DOMOVSKÁ STRÁNKA ***/}
                 <Route path="/" element={ <Home /> } />
                 <Route path="/home" element={ <Home /> } />
+
+                { /*** KONTAKT ***/}
                 <Route path="/kontakt" element={ <Kontakt /> } />
                 <Route path="/contact" element={ <Kontakt /> } />
+
+                { /*** VŠECHNY PROJEKTY ***/}
                 <Route path="/projekty" element={ <Projekty /> } />
                 <Route path="/projects" element={ <Projekty /> } />
+
+                { /*** JEDEN PROJEKT ***/}
+                <Route path="/projekty/:projectName" element={ <ProjectPage /> } />
+                <Route path="/projects/:projectName" element={ <ProjectPage /> } />
+                <Route path="/projekt/:projectName" element={ <ProjectPage /> } />
+                <Route path="/project/:projectName" element={ <ProjectPage /> } />
+
+
+                { /*** ***/}
                 <Route path="*" element={ <NotFound /> } />
             </Routes>
             <Footer/>
---
+++ src/pages/ProjectPage.js
@@ -0,0 +1,74 @@
+import React, { useEffect } from "react";
+import {Link, useNavigate, useParams} from "react-router-dom";
+import "./ProjectPage.css";
+import ProjectList from "../contentData/projects";
+import homepageTechnologiesList from "../contentData/homepageTechnologiesList";
+
+const ProjectPage = () => {
+    const navigate = useNavigate();
+    const { projectName } = useParams();
+    const project = ProjectList.find((project) => project.title === projectName);
+
+    useEffect(() => {
+        if (project === undefined) {
+            navigate("/404");
+        }
+    }, [project, navigate]);
+
+    return (
+        <section id="projectPage">
+            <div className="container">
+                <div className="Icon box">
+                    <img src={project.img} alt="Ikona projektu" />
+                </div>
+                <div className="Description box">
+                    <p style={{ whiteSpace: "pre-line" }}>{project.description}</p>
+                </div>
+                <div className="Title box">
+                    <h2>{project.title}</h2>
+                </div>
+                <div className="Activity box">
+                    <h4>
+                        {project.startedDate.month + "/" + project.startedDate.year} -{" "}
+                        {project.endedDate.month === new Date().getMonth() + 1 &&
+                        project.endedDate.year === new Date().getFullYear()
+                            ? "Aktivní"
+                            : project.endedDate.month + "/" + project.endedDate.year}
+                    </h4>
+                </div>
+                <div className="Tech-stack box">
+                    <h4>Použité technologie:</h4>
+                    <span className="techStackIcons">
+            {project.techStack.map((tech, index) => {
+                const techInfo = homepageTechnologiesList.find(
+                    (t) => t.name === tech
+                );
+
+                return (
+                    <React.Fragment key={tech}>
+                        {index > 0 && <span style={{ color: "white" }}>, </span>}
+                        <span style={{ color: techInfo?.color || "inherit" }}>
+                    {techInfo?.iconComponent &&
+                        React.createElement(techInfo.iconComponent, {
+                            style: { marginRight: 4 },
+                        })}
+                            {tech}
+                  </span>
+                    </React.Fragment>
+                );
+            })}
+          </span>
+                </div>
+                <div className="Links box">
+                    <h4>Odkazy:</h4>
+                    {project.links.map((link, index) => {
+                        return <a href={link.link} key={index} className="projectLink">{link.label}</a>;
+                    })}
+                    <Link to="/projekty" className="projectLink">Zpět na projekty</Link>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default ProjectPage;

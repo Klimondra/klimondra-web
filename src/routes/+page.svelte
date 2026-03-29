6606d527e4b5f02cf12066da8e956996b7d3cd80
Klimondra <klimondra@seznam.cz>
2026-03-29 14:08:09 +0200

    feature: Added about me section concept

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
+	import AboutSection from '$lib/components/features/home/AboutSection.svelte';
     import HeroSection from '$lib/components/features/home/HeroSection.svelte';
 </script>
 
-<HeroSection />
+<main class="min-h-screen flex flex-col gap-12">
+    <HeroSection />
+    <AboutSection />
+</main>

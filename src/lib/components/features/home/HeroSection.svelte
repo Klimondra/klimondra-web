024e13009d6984501159b8ccf18d9b7ddb22907f
Klimondra <klimondra@seznam.cz>
2026-03-30 16:21:52 +0200

    feature: Restyled hero section

--- src/lib/components/features/home/HeroSection.svelte
+++ src/lib/components/features/home/HeroSection.svelte
@@ -3,11 +3,14 @@
     import Section from '$lib/components/ui/Section.svelte';
 </script>
 
-<Section class="h-screen pt-18 mt-14 pb-8">
+<Section class="h-screen mt-6 py-20">
     <!-- TODO: Add background gradient -->
-    <div class="w-full h-full px-8 py-24 flex flex-col justify-start items-center gap-3 bg-slate-900 rounded-3xl border border-neutral-800">
-        <h2 class="text-3xl sm:text-4xl md:text-5xl font-bold text-white text-center">Dělám weby, které fungují</h2>
-        <p class="text-base sm:text-lg md:text-xl font-light text-neutral-100 tracking-wide text-center">Profesionální weby, které vás odlišují a přinášejí výsledky.</p>
+    <div 
+        class="w-full h-full p-8 flex flex-col justify-center items-center gap-4
+            bg-linear-to-br from-azure-950 via-azure-900 to-azure-800 rounded-3xl"
+    >
+        <h2 class="text-5xl sm:text-6xl md:text-7xl font-bold text-white text-center">Dělám weby, které fungují</h2>
+        <p class="text-base sm:text-lg md:text-2xl font-light text-neutral-100 tracking-wide text-center">Profesionální weby, které vás odlišují a přinášejí výsledky.</p>
         <div class="flex flex-col sm:flex-row gap-4 mt-3">
             <Button variant="primary">Zjistit více</Button>
             <Button variant="secondary">Zavolat</Button>

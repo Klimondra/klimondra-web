091a8b794468370aaa8712e70ddfcd5ae578826b
Klimondra <klimondra@seznam.cz>
2026-03-30 19:19:38 +0200

    feature: Fixed z index in hero

--- src/lib/components/features/home/HeroSection.svelte
+++ src/lib/components/features/home/HeroSection.svelte
@@ -60,15 +60,15 @@
         class="w-full h-full relative p-8 flex flex-col justify-center items-center gap-6
             bg-linear-to-br from-azure-950 via-azure-900 to-azure-800 rounded-3xl overflow-hidden invisible"
     >
-        <div class="flex flex-col gap-3">
+        <div class="absolute top-0 left-0 w-full h-full bg-linear-to-b from-transparent via-transparent to-azure-800 blur-2xl -z-10"></div>
+        <div class="flex flex-col gap-3 z-0">
             <h2 class="text-5xl sm:text-6xl md:text-7xl font-bold text-white text-center">Dělám weby, které fungují</h2>
             <p class="text-base sm:text-lg md:text-2xl font-light text-neutral-100 tracking-wide text-center">Profesionální weby, které vás odlišují a přinášejí výsledky.</p>
         </div>
-        <div class="flex flex-col sm:flex-row gap-4 mt-3">
+        <div class="flex flex-col sm:flex-row gap-4 mt-3 z-0">
             <Button variant="primary">Zjistit více</Button>
             <Button variant="secondary">Zavolat</Button>
         </div>
-        <div class="absolute top-0 left-0 w-full h-full bg-linear-to-b from-transparent via-transparent to-azure-800 blur-3xl"></div>
         <div bind:this={photosContainer} class="absolute -bottom-8 w-full h-36 flex items-center justify-center gap-8">
             {#each heroImages as image, index (index)}
                 <img src={image.src} alt={image.alt} class={"animated-hero-image h-36 rounded-sm " + (index % 2 === 0 ? "-rotate-3" : "rotate-3")} />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
 	import AboutSection from '$lib/components/features/home/AboutSection.svelte';
     import HeroSection from '$lib/components/features/home/HeroSection.svelte';
+	import Section from '$lib/components/ui/Section.svelte';
 </script>
 
-<main class="min-h-screen flex flex-col gap-12">
+<main class="min-h-screen flex flex-col gap-8">
     <HeroSection />
     <AboutSection />
+    <Section><h1>Section</h1></Section>
 </main>
